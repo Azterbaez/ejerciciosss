e7aba0bfae178674ec8cc272493ca8720c769627
Azterbaez <rivasmingito48@gmail.com>
2025-09-09 18:19:20 +0200

    rutas de obtener por id y insert

--- src/routes/categorias.routes.js
+++ src/routes/categorias.routes.js
@@ -1,10 +1,15 @@
 import { Router } from 'express';
-import {obtenerCategorias} from '../controllers/categorias.controllers.js';
+import {obtenerCategorias, obtenerCategoria,registrarCategoria} from '../controllers/categorias.controllers.js';
 
 const router = Router();
 
 // Rutas
 router.get ('/categorias', obtenerCategorias);
+// ruta por id
+router.get ('/categoria/:id_categoria',obtenerCategoria); 
+
+// Ruta para registrar una nueva Categoría
+router.post('/registrarcategoria', registrarCategoria);
 
 export default router;
 

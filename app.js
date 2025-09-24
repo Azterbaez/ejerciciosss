f4bbd37593ff7375cbaab62ea361cec1cd49eec3
azterbaez <rivasmingito48@gmail.com>
2025-09-24 03:50:37 +0200

    estructura de los demas controllers y rutes

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ import express from 'express';
 import cors from 'cors';
 
 import rutasCategorias from'./src/routes/categorias.routes.js';
+import rutasClientes from './src/routes/clientes.routes.js'
+import rutasCompras from './src/routes/compras.routes.js'
+import rutasDetalles_compras from './src/routes/detalles_compras.routes.js'
+import rutasDetalles_ventas from './src/routes/detalles_ventas.routes.js'
+import rutasEmpleados from './src/routes/empleados.routes.js'
+import rutasProductos from './src/routes/productos.routes.js'
+import rutasUsuarios from './src/routes/usuarios.routes.js'
+import rutasVentas from './src/routes/ventas.routes.js'
 
 const app = express();
 

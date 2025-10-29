e6e9154dcf0229fbce5065b938804f57a8858ca8
azterbaez <rivasmingito48@gmail.com>
2025-10-29 11:36:41 -0600

    mejoras

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,98 +1,20 @@
-import { pool } from '../../db_connection.js';
+import { Router } from 'express';
+import { obtenerUsuarios, obtenerUsuario, registrarUsuario, eliminarUsuario, actualizarusuariosPatch } from '../controllers/usuarios.controllers.js';
 
-// Obtener todas las categorías
-export const obtenerUsuarios = async (req, res) => {
-  try {
-    const [result] = await pool.query('SELECT * FROM usuarios');
-    res.json(result);
-  } catch (error) {
-    return res.status(500).json({
-      mensaje: 'Ha ocurrido un error al leer los datos.',
-      error: error
-    });
-  }
-};
+const router = Router();
 
-// Obtener una Usuario por su ID
-export const obtenerUsuario = async (req, res) => {
-  try {
-    const id_usuario = req.params.id_usuario;
-    const [result] = await pool.query('SELECT * FROM usuarios WHERE id_usuario= ?', [req.params.id_usuario])
-    if (result.length <= 0) {
-      return res.status(404).json({
-        mensaje: "Error al leer los datos. ID ${id_usuario} no encontrado."
-      });
-    }
-    res.json(result[0]);
-  } catch (error) {
-    return res.status(500).json({
-      mensaje: 'Ha ocurrido un error al leer los datos de las usuarios.'
-    });
-  }
-};
+router.get('/usuarios', obtenerUsuarios);
 
-// Registrar una nueva Compra
-export const registrarUsuario = async (req, res) => {
-  try {
-    const { usuario, contraseña } = req.body;
-    const [result] = await pool.query(
-      'INSERT INTO Usuarios (usuario, contraseña) VALUES (?, ?)',
-      [ usuario, contraseña ]
-    );
-    res.status(201).json({ id_usuario: result.insertId });
-  } catch (error) {
-    return res.status(500).json({
-      mensaje: 'Ha ocurrido un error al registrar la usuarios.',
-      error: error
-    });
-  }
-};
+// Rutas
+router.get('/usuarios/:id_usuario', obtenerUsuario);
 
-// Eliminar una Usuario por su ID 
-export const eliminarUsuario = async (req, res) =>  {
-  try {
-    const id_usuario = req.params.id_usuario;
-    const [result] = await pool.query('DELETE FROM usuarios WHERE id_usuario= ?', [id_usuario]);
+// Ruta para registrar una nueva Empleado
+router.post('/registrarUsuario', registrarUsuario);
 
-    if (result.affectedRows === 0) {
-      return res.status(404).json({
-        mensaje: "Error al eliminar la usuarios. ID ${id_usuario} no fue encontrado."
-      });
-    }
+//Rutas
+router.delete('/eliminarUsuario/:id_usuario', eliminarUsuario);
 
-    res.status(200).json({
-      mensaje: "La usuarios con ID ${id_usuario} fue eliminada correctamente."
-    });
-  } catch (error) {
-    return res.status(500).json({
-      mensaje: 'Ha ocurrido un error al eliminar la usuarios.',
-      error: error
-    });
-  }
-};
+// Ruta para actualizar una Usuario por su ID
+router.patch('/actualizarusuariosPatch/:id_usuario', actualizarusuariosPatch);
 
-
-// Controlador para actualizar parcialmente una usuarios por su ID
-export const actualizarusuariosPatch = async (req, res) => {
-  try {
-    const { id_usuario } = req.params;
-    const datos = req.body;
-
-    const [result] = await pool.query(
-      'UPDATE usuarios SET ? WHERE id_usuario = ?',
-      [datos, id_usuario]
-    );
-
-    if (result.affectedRows === 0) {
-      return res.status(404).json({
-        mensaje: "usuarios con ID ${id_usuario} no encontrada."
-      });
-    }
-
-    res.status(200).json({
-      mensaje: "usuarios con ID ${id_usuario} actualizada."
-    });
-  } catch (error) {
-    res.status(500).json({ mensaje: 'Error al actualizar la usuarios.', error });
-  }
-};
+export default router;

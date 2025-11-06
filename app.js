c80a55762fb7c6c3d42124bbd95c561ac7ef3610
azterbaez <rivasmingito48@gmail.com>
2025-11-06 10:52:38 -0600

    empleados coregidos

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import rutasCategorias from'./src/routes/categorias.routes.js';
 import rutasClientes from'./src/routes/clientes.routes.js';
 import rutasUsuarios from'./src/routes/usuarios.routes.js';
+import rutasEmpleados from'./src/routes/empleados.routes.js';
 
 
 const app = express();
@@ -19,6 +20,7 @@ app.use(express.urlencoded({ limit: '10mb', extended: true}));
 app.use('/api', rutasCategorias);
 app.use('/api', rutasClientes);
 app.use('/api', rutasUsuarios);
+app.use('/api', rutasEmpleados);
 
 app.use((req, res, next)=>{
     res.status(404).json({
---
+++ src/routes/empleados.routes.js
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+import {obtenerEmpleados, obtenerEmpleado,registrarEmpleado, eliminarEmpleado, actualizarEmpleado,} from '../controllers/empleados.controllers.js';
+
+const router = Router();
+
+// Rutas
+router.get ('/empleados', obtenerEmpleados);
+// ruta por id
+router.get ('/empleado/:id_empleado',obtenerEmpleado);
+
+// Ruta para registrar una nueva Categoría
+router.post('/registrarempleado', registrarEmpleado);
+
+router.delete('/eliminarempleado/:id_empleado',eliminarEmpleado);
+
+// Ruta para actualizar una Categoría por su ID
+router.patch('/actualizarempleado/:id_empleado', actualizarEmpleado);
+
+
+
+export default router;
